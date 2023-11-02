f388b9593d9d794a16d20f0fc2a251b1771af9ae
gyojincompany <luckyred@gmail.com>
2023-11-02 11:14:26 +0900

    구글 번역 결과 출력 기능 추가

--- googleTransApp.py
+++ googleTransApp.py
@@ -17,6 +17,27 @@ class MyGoogleTrans(QMainWindow, form_class):
         self.setWindowIcon(QIcon('icon/google.png')) # 번역기 앱의 아이콘
         self.statusBar().showMessage('Google Trans App v1.0 Copyright ⓒ GyojinCompany') # 상태 표시줄
 
+        self.btn_trans.clicked.connect(self.trans_function) #signal
+
+
+    def trans_function(self): #slot
+        trans_kor = self.input_kor_text.text() # input_kor_text 에 입력된 한글을 가져옴
+
+        trans = googletrans.Translator() # 구글트랜스 모듈의 객체(번역해 주는 객체)
+
+        # print(googletrans.LANGUAGES) # 번역 언어 ticker 불러오기
+
+        trans_eng = trans.translate(trans_kor, dest='en') # 영어번역결과
+        trans_jap = trans.translate(trans_kor, dest='ja') # 일본어번역결과
+        trans_chn = trans.translate(trans_kor, dest='zh-cn') # 중국어번역결과
+
+        self.output_eng_text.append(trans_eng.text)
+        self.output_jap_text.append(trans_jap.text)
+        self.output_chn_text.append(trans_chn.text)
+
+    def reset_kor(self): #slot
+        self.input_kor_text.clear()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
